95112f363a5bb42c9a394f2f63c69dc12d3de28f
Anand Ohri <78968462+anandohri@users.noreply.github.com>
2021-05-05 23:15:49 +0530

    Add files via upload

--- connect4_server/index.js
+++ connect4_server/index.js
@@ -6,10 +6,29 @@ const server = http.createServer();
 server.listen(webSocketsServerPort);
 console.log('listening to port 8000');
 
+const player = {};
+let numOfPlayer = 0;
+
 const wsServer = new webSocketServer({
   httpServer: server
 });
 
 wsServer.on('request', function(request){
     console.log('New user connected');
+    if(numOfPlayer < 2){
+      numOfPlayer += 1;
+      connection = request.accept(null, request.origin);
+      player[numOfPlayer] = connection;
+      console.log('Connected: ' + numOfPlayer + ' with ' + Object.getOwnPropertyNames(player));
+
+      connection.on('message', function(message){
+        if(message.type === 'utf8'){
+          console.log(message.utf8Data + ' Joined')
+          for(key in player){
+            player[key].sendUTF(message.utf8Data);
+            console.log('Sent message to: ', player[key]);
+          }
+        }
+      });
+    }
 });
