9812310070e1811874927c29e348f5e171ef3d18
Anand Ohri <78968462+anandohri@users.noreply.github.com>
2021-05-05 23:16:10 +0530

    Add files via upload

--- connect4_client/src/index.js
+++ connect4_client/src/index.js
@@ -6,14 +6,42 @@ import {w3cwebsocket as W3CWebsocket} from 'websocket';
 const client = new W3CWebsocket ('ws://192.168.0.199:8000');
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {name: ''}
+  }
+
+  handleChange = (e) => {
+    this.setState({name: e.target.value})
+  }
+
+  handleClick = (val) => {
+    client.send(JSON.stringify({
+      type: "message",
+      msg: val
+    }));
+  }
+
   componentDidMount(){
     client.onopen = () => {
       console.log('connected');
     };
+
+    client.onmessage = (message) => {
+      const nameFromServer = JSON.parse(message.data);
+      console.log('From Server: ', nameFromServer);
+    };
   }
 
   render(){
-    return 'Testing';
+    return(
+      <div>
+        <input onChange = {this.handleChange} value = {this.state.name} label = "Enter Name" />
+        <button onClick = {() => this.handleClick(this.state.name)}>
+          Submit
+        </button>
+      </div>
+    )
   }
 }
 
