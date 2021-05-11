2105d7080e48b1ea200230607769022c9a64a832
Anand Ohri <78968462+anandohri@users.noreply.github.com>
2021-05-11 23:34:21 +0530

    Add files via upload

--- connect4_client/src/index.js
+++ connect4_client/src/index.js
@@ -12,7 +12,67 @@ class App extends React.Component{
                   uname: '',
                   isloggedin: false,
                   moves: {},
-                  prev: 0}
+                  prev: 0,
+                  isWinner: 'NA'}
+  }
+
+  calcWinner = (current) => {
+    const currPlayer = 'player' + current;
+    const moves = this.state.moves;
+    let found = 'n';
+    for(let i = 5; i >= 0; --i){
+      for(let j = 1; j <= 7; ++j){
+        const ind = (i * 7) + j;
+        if(moves[ind] == currPlayer &&
+            moves[ind + 1] == currPlayer &&
+            moves[ind + 2] == currPlayer &&
+            moves[ind + 3] == currPlayer){
+          moves[ind] = 'winner' + current;
+          moves[ind + 1] = 'winner' + current;
+          moves[ind + 2] = 'winner' + current;
+          moves[ind + 3] = 'winner' + current;
+          found = 'y';
+          break;
+        }
+        else if(moves[ind] == currPlayer &&
+                  moves[ind - 7] == currPlayer &&
+                  moves[ind - 14] == currPlayer &&
+                  moves[ind - 21] == currPlayer){
+          moves[ind] = 'winner' + current;
+          moves[ind - 7] = 'winner' + current;
+          moves[ind - 14] = 'winner' + current;
+          moves[ind - 21] = 'winner' + current;
+          found = 'y';
+          break;
+        }
+        else if(moves[ind] == currPlayer &&
+                  moves[ind - 6] == currPlayer &&
+                  moves[ind - 12] == currPlayer &&
+                  moves[ind - 18] == currPlayer){
+          moves[ind] = 'winner' + current;
+          moves[ind - 6] = 'winner' + current;
+          moves[ind - 12] = 'winner' + current;
+          moves[ind - 18] = 'winner' + current;
+          found = 'y';
+          break;
+        }
+        else if(moves[ind] == currPlayer &&
+                  moves[ind - 8] == currPlayer &&
+                  moves[ind - 16] == currPlayer &&
+                  moves[ind - 24] == currPlayer){
+          moves[ind] = 'winner' + current;
+          moves[ind - 8] = 'winner' + current;
+          moves[ind - 16] = 'winner' + current;
+          moves[ind - 24] = 'winner' + current;
+          found = 'y';
+          break;
+        }
+      }
+      if(found === 'y'){
+        this.setState({moves: moves, isWinner: currPlayer});
+        break;
+      }
+    }
   }
 
   renderBoard = () => {
@@ -29,6 +89,16 @@ class App extends React.Component{
         else if(this.state.moves[ind] === 'player2'){
           rowItem.push(<div className = 'boardCell'>
                           <button className = 'player2' onClick = {() => this.handleMove(i, j)} />
+                        </div>)
+        }
+        else if(this.state.moves[ind] === 'winner1'){
+          rowItem.push(<div className = 'winnerCell'>
+                          <button className = 'player1' />
+                        </div>)
+        }
+        else if(this.state.moves[ind] === 'winner2'){
+          rowItem.push(<div className = 'winnerCell'>
+                          <button className = 'player2' />
                         </div>)
         }
         else {
@@ -56,21 +126,23 @@ class App extends React.Component{
 
   handleMove = (i, j) => {
     const id = (7 * i) + j;
-    if(i === 5){
-      client.send(JSON.stringify({
-        type: "message",
-        user: this.state.uname,
-        move: id
-      }))
-    }
-    else{
-      const lower = (7 * (i + 1)) + j;
-      if(this.state.moves[lower] === 'player1' || this.state.moves[lower] === 'player2'){
+    if(this.state.isWinner === 'NA'){
+      if(i === 5){
         client.send(JSON.stringify({
           type: "message",
           user: this.state.uname,
           move: id
         }))
+      }
+      else{
+        const lower = (7 * (i + 1)) + j;
+        if(this.state.moves[lower] === 'player1' || this.state.moves[lower] === 'player2'){
+          client.send(JSON.stringify({
+            type: "message",
+            user: this.state.uname,
+            move: id
+          }))
+        }
       }
     }
   }
@@ -89,19 +161,22 @@ class App extends React.Component{
       else if(dataFromServer.type === 'message' && dataFromServer.user === this.state.uname && this.state.pnum !== this.state.prev) {
         const move = this.state.moves;
         move[dataFromServer.move] = 'player' + this.state.pnum;
-        this.setState({moves: move, prev: this.state.pnum})
+        this.setState({moves: move, prev: this.state.pnum});
+        this.calcWinner(this.state.pnum);
       }
       else if(dataFromServer.type === 'message' && dataFromServer.user !== this.state.uname &&
                 (this.state.pnum === this.state.prev || this.state.prev === 0)) {
-        if(this.state.pnum == 1){
+        if(this.state.pnum === 1){
           const move = this.state.moves;
           move[dataFromServer.move] = 'player2';
-          this.setState({moves: move, prev: 2})
+          this.setState({moves: move, prev: 2});
+          this.calcWinner(2);
         }
-        else if(this.state.pnum == 2){
+        else if(this.state.pnum === 2){
           const move = this.state.moves;
           move[dataFromServer.move] = 'player1';
-          this.setState({moves: move, prev: 1})
+          this.setState({moves: move, prev: 1});
+          this.calcWinner(1);
         }
       }
     };
